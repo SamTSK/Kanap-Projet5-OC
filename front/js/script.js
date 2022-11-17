bdd45cbf4090ca42fb05b9e11655852fb2ba838c
Samira Tahirou <tahirousamira@gmail.com>
2022-11-17 23:50:41 +0100

    Products

---
+++ front/js/script.js
@@ -0,0 +1,8 @@
+let products = `<a href="./product.html?id=42">
+            <article>
+              <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">Kanap name1</h3>
+              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+            </article>
+          </a>`
+document.getElementsByClassName ('items')[0].innerHTML
