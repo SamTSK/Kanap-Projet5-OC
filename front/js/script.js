18058c479c0bbcbf52da1d7900face8d319a6c83
Samira Tahirou <tahirousamira@gmail.com>
2022-11-23 09:12:34 +0100

    Adding package.json & products

--- front/js/script.js
+++ front/js/script.js
@@ -1,8 +1,26 @@
-let products = `<a href="./product.html?id=42">
+let template = `<a href="./product.html?id=42">
             <article>
               <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
               <h3 class="productName">Kanap name1</h3>
               <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
             </article>
           </a>`
-document.getElementsByClassName ('items')[0].innerHTML = products
+//document.getElementsByClassName ('items')[0].innerHTML = template;
+
+fetch ("http://localhost:3000/api/products")
+  .then (function (response) {
+    return response.json()
+  })
+  .then (function (products) {
+    products.map((el,i)=>{
+      items.innerHTML +=
+      `<a href="./product.html?id=${el._id}">
+            <article>
+              <img src ="${el.imageUrl}" alt="${el.altTxt}">
+              <h3 class="productName">Kanap ${el.name}</h3>
+              <p class="productDescription">${el.description}Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+            </article>
+          </a>`
+    })
+  })
+  
