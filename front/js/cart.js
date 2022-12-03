408b3b81893185d01794b83f7b28943f23fe3f10
Samira Tahirou <tahirousamira@gmail.com>
2022-12-03 01:49:57 +0100

    work in progress

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,15 @@
 const cart = []
-
 retrieveItemsFromCache()
+
 cart.forEach((item) => showItem(item))
+
+/*altTxt : "Photo d'un canapé d'angle, vert, trois places"
+color : "Green"
+id : "055743915a544fde83cfdfc904935ee7"
+imageUrl : "http://localhost:3000/images/kanap03.jpeg"
+quantity: 3
+*/
+
 
 function retrieveItemsFromCache(){
     const numberOfItems = localStorage.length
