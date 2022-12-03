61c46b41b0c4f18d166095316883d6683ed93385
Samira Tahirou <tahirousamira@gmail.com>
2022-12-03 01:43:09 +0100

    working on cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,1 +1,18 @@
 const cart = []
+
+retrieveItemsFromCache()
+cart.forEach((item) => showItem(item))
+
+function retrieveItemsFromCache(){
+    const numberOfItems = localStorage.length
+    for (let i = 0; i < numberOfItems; i++){
+        const item = localStorage.getItem(localStorage.key(i))
+        const itemObject = JSON.parse(item)
+        cart.push(itemObject)
+    }
+}
+
+function showItem(item){
+    const itemObject = JSON.parse(item)
+    
+}
